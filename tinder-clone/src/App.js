b6e72e1ac471412c391a1ec31af5670ef7526284
Abhinit Patil <abhineet0309@gmail.com>
2021-01-08 01:04:22 -0500

    first commit

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    //BEM class namin convention
+    <div className="app">
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
